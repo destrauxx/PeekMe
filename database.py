3792c04bcc28968a33336f88ca9e1a893a51be96
destrauxx <gleroapachii@gmail.com>
2025-03-14 15:50:04 +0300

    some find functions in dbhandler

--- database.py
+++ database.py
@@ -24,6 +24,18 @@ class DatabaseHandler():
         self.cursor.execute('INSERT INTO Users (username, description, age, interests, rating, type, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)', (username, description, age, interests, rating, type, image_url))
         self.database.commit()
         
+    def find_users_by_username(self, username):
+        self.cursor.execute("SELECT * FROM Users WHERE username = ?", (username))
+        return self.cursor.fetchall()
+        
+    def find_users_by_type(self, type):
+        self.cursor.execute("SELECT * FROM Users WHERE type = ?", (type))
+        return self.cursor.fetchall()
+    
+    def find_users_by_rating(self, rating):
+        self.cursor.execute("SELECT * FROM Users WHERE rating = ?", (rating))
+        return self.cursor.fetchall()
+    
     def __del__(self):
         self.database.close()
         
